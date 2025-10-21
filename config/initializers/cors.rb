83637623c6473a9e5eaf7998205fd6c1ccf296e7
Realmunene <onlinemunene@gmail.com>
2025-10-21 12:09:00 +0300

    modified origin lin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://realmunene.github.io/southcoastwebmain/'
 
     resource "*",
       headers: :any,
