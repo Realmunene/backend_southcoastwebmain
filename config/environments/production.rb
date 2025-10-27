790c8049e395b00e306c784129d77055e499cba5
Realmunene <onlinemunene@gmail.com>
2025-10-27 19:39:35 +0300

    fixing 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.assume_ssl = true
   config.force_ssl = true
 
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.logger   = ActiveSupport::Logger.new(STDOUT)
   config.log_level = :debug
 
@@ -46,14 +46,15 @@ Rails.application.configure do
 
   config.active_support.report_deprecations = false
 
-  config.cache_store = :solid_cache_store
+  # Removed solid_cache_store
+  # config.cache_store = :solid_cache_store
 
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # Removed solid_queue
+  config.active_job.queue_adapter = :inline
 
   config.i18n.fallbacks = true
   config.active_record.dump_schema_after_migration = false
-  config.active_record.attributes_for_inspect = [ :id ]
+  config.active_record.attributes_for_inspect = [:id]
 
   ##############################################
   # ✅ ActionMailer configured to use RESEND SMTP
